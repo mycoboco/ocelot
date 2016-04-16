6f1b327a17c8914ffc20f78cfb1f9a5ac5827229
Jun Woong <woong.jun@gmail.com>
2016-04-16 20:02:08 +0900

    Initial commit for dwa

---
+++ src/cdsl/dwa.c
@@ -0,0 +1,9 @@
+/*
+ *  double-word arithmetic (cdsl)
+ */
+
+#include "cbl/assert.h"    /* assert with exception support */
+#include "dwa.h"
+
+
+/* end of dwa.c */
---
+++ src/cdsl/dwa.h
@@ -0,0 +1,12 @@
+/*
+ *  double-word arithmetic (cdsl)
+ */
+
+#ifndef DWA_H
+#define DWA_H
+
+
+
+#endif    /* DWA_H */
+
+/* end of dwa.h */
